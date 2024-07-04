8283b01e56a7876e53d6ccd43a571cd9de9c3b32
vishnupriyavellingiri <161208547+vishnupriyavellingiri@users.noreply.github.com>
2024-07-04 21:31:10 +0530

    Update AuthController.java

--- Spring_boot_JWT-master/src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ Spring_boot_JWT-master/src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -31,7 +31,7 @@ import com.bezkoder.springjwt.repository.RoleRepository;
 import com.bezkoder.springjwt.repository.UserRepository;
 import com.bezkoder.springjwt.security.jwt.JwtUtils;
 import com.bezkoder.springjwt.security.services.UserDetailsImpl;
-//vishnu changes
+//vishnu changes commited
 @CrossOrigin(origins = "*", maxAge = 3600) //  Cross-Origin Resource Sharing (CORS) 
 @RestController
 @RequestMapping("/api/auth")
